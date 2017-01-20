135c75f59d271644db04e57ea195e0afadd8c1f2
Renato Maia <maia@tecgraf.puc-rio.br>
2017-01-20 01:00:31 -0200

    Removing stuff of other modules originally in the same LOOP repository.

--- test/testall.lua
+++
@@ -1,6 +0,0 @@
-local Runner = require "loop.test.Results"
-local Reporter = require "loop.test.Reporter"
-local runner = Runner{ reporter = Reporter{ time = socket and socket.gettime } }
-runner("LOOP", require("loop.tests.Suite"), runner)
-
-require("cothread.tests.Suite")
